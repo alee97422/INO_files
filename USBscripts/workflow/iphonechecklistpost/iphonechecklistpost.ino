6f7f464d16e33fb895f72f145bfbc989718a9306
alee97422 <alee97422@gmail.com>
2023-07-03 22:26:04 -0500

    adding the first addition to my workflow scripts with a automation script for my post check iphone inspection checklist

---
+++ USBscripts/workflow/iphonechecklistpost/iphonechecklistpost.ino
@@ -0,0 +1,146 @@
+#include <Keyboard.h>
+
+void setup() {
+  // Initialize Keyboard
+  Keyboard.begin();
+
+  // Delay for the computer to recognize the device
+  delay(2000);
+
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  
+  delay(500);
+
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_DOWN_ARROW);
+  Keyboard.releaseAll();
+  delay(500);
+
+
+  
+
+  // Wait for the alert to display
+  delay(3000);
+
+  // Stop the keyboard
+  Keyboard.end();
+}
+
+void loop() {
+  // Empty loop
+}
